df9ec3ec6e4b64a7c2516802c03bf08ad3bef980
manjunathramesh <manjus271@gmail.com>
2020-06-03 11:46:06 -0500

    Update appcenter-pre-build.sh

--- android/app/appcenter-pre-build.sh
+++ android/app/appcenter-pre-build.sh
@@ -1,4 +1,4 @@
 dig +short myip.opendns.com @resolver1.opendns.com
-npm config set registry https://jfrog.devops.7-11.com/artifactory/api/npm/npm
+
 rm -rf node_modules && npm install --ignore-engines
 cd ../../ && npm run bundle --ignore-engines
